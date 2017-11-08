6f3557134c72d96c164249b0e03a93b6cc77535b
Taufik Nurrohman <t.nurrohman77@gmail.com>
2017-11-08 20:06:40 +0700

    Updates for @mecha-cms/genome

--- contact/lot/worker/form.php
+++ contact/lot/worker/form.php
@@ -5,7 +5,7 @@
   <?php if (is_array($lot['topic'])): ?>
   <p class="form-contact-select form-contact-select:title">
     <label for="form-contact-select:title"><?php echo $language->contact_title; ?></label>
-    <span><?php echo Form::select('*title', array_merge(["" => $language->contact_f_title], $b['topic']), null, ['classes' => ['select', 'block'], 'id' => 'form-contact-select:title']); ?></span>
+    <span><?php echo Form::select('*title', array_merge(["" => $language->contact_f_title], $b['topic']), null, ['class[]' => ['select', 'block'], 'id' => 'form-contact-select:title']); ?></span>
   </p>
   <?php else: ?>
   <?php echo Form::hidden('title', $lot['topic']); ?>
@@ -13,30 +13,30 @@
   <?php else: ?>
   <p class="form-contact-input form-contact-input:title">
     <label for="form-contact-input:title"><?php echo $language->contact_title; ?></label>
-    <span><?php echo Form::text('*title', null, $language->contact_f_title, ['classes' => ['input', 'block'], 'id' => 'form-contact-input:title']); ?></span>
+    <span><?php echo Form::text('*title', null, $language->contact_f_title, ['class[]' => ['input', 'block'], 'id' => 'form-contact-input:title']); ?></span>
   </p>
   <?php endif; ?>
   <p class="form-contact-input form-contact-input:author">
     <label for="form-contact-input:author"><?php echo $language->contact_author; ?></label>
-    <span><?php echo Form::text('*author', null, $language->contact_f_author, ['classes' => ['input', 'block'], 'id' => 'form-contact-input:author']); ?></span>
+    <span><?php echo Form::text('*author', null, $language->contact_f_author, ['class[]' => ['input', 'block'], 'id' => 'form-contact-input:author']); ?></span>
   </p>
   <p class="form-contact-input form-contact-input:email">
     <label for="form-contact-input:email"><?php echo $language->contact_email; ?></label>
-    <span><?php echo Form::email('*email', null, $language->contact_f_email, ['classes' => ['input', 'block'], 'id' => 'form-contact-input:email']); ?></span>
+    <span><?php echo Form::email('*email', null, $language->contact_f_email, ['class[]' => ['input', 'block'], 'id' => 'form-contact-input:email']); ?></span>
   </p>
   <p class="form-contact-input form-contact-input:link">
     <label for="form-contact-input:link"><?php echo $language->contact_link; ?></label>
-    <span><?php echo Form::url('link', null, $language->contact_f_link, ['classes' => ['input', 'block'], 'id' => 'form-contact-input:link']); ?></span>
+    <span><?php echo Form::url('link', null, $language->contact_f_link, ['class[]' => ['input', 'block'], 'id' => 'form-contact-input:link']); ?></span>
   </p>
   <div class="form-contact-textarea form-contact-textarea:content p">
     <label for="form-contact-textarea:content"><?php echo $language->contact_content; ?></label>
-    <div><?php echo Form::textarea('*content', null, $language->contact_f_content, ['classes' => ['textarea', 'block'], 'id' => 'form-contact-textarea:content']); ?></div>
+    <div><?php echo Form::textarea('*content', null, $language->contact_f_content, ['class[]' => ['textarea', 'block'], 'id' => 'form-contact-textarea:content']); ?></div>
   </div>
   <?php if (isset($lot['kick'])): ?>
   <?php echo Form::hidden('kick', $lot['kick']); ?>
   <?php endif; ?>
   <p class="form-contact-button form-contact-button:state">
     <label for="form-contact-button:state"></label>
-    <span><?php echo Form::submit('state', null, $language->contact_submit, ['classes' => ['button', 'button-submit', 'set'], 'id' => 'form-contact-button:state']); ?></span>
+    <span><?php echo Form::submit('state', null, $language->contact_submit, ['class[]' => ['button', 'button-submit', 'set'], 'id' => 'form-contact-button:state']); ?></span>
   </p>
 </form>
